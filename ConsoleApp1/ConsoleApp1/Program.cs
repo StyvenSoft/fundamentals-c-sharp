88f21412b9d2513c170f0b8f91ff770720413ee9
Styvensoft <steveen.echeverri@gmail.com>
2022-05-30 22:39:04 -0500

    Seconds examples basics

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -44,6 +44,58 @@ namespace ConsoleApp1
 
             Console.WriteLine(agePerson3);
 
+            // Conversion explicita
+            // Casting
+
+            double temperature = 34.5;
+            int temperatureCol;
+
+            temperatureCol = (int)temperature;
+
+            Console.WriteLine(temperatureCol);
+
+            // Conversion implicita
+
+            int populationBog = 12000000;
+
+            long populationContries = populationBog;
+
+            Console.WriteLine(populationContries);
+
+            float materialWeight = 5.78F;
+
+            double materialWeightPrec = materialWeight;
+
+            Console.WriteLine(materialWeightPrec);
+
+            // Type Conversion
+            // Methodo parse()
+
+            Console.WriteLine("Ingresa numero: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa segundo numero: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            //Uging
+
+            Console.WriteLine("Resultado es: " + (num1 + num2));
+
+            const int valueOne = 4;
+            Console.WriteLine(valueOne);
+
+            // Constante
+
+            const double PI = 3.1413;
+
+            Console.WriteLine("Ingresa medida radio: ");
+
+            double radie = double.Parse(Console.ReadLine());
+
+            //double area = radie * radie * PI;
+            double area = Math.Pow(radie, 2) * PI;
+
+            Console.WriteLine("Area del circulo es: " + area);
 
             Console.ReadKey();
         }
