b93fc7b5e4e8b0a47f909b9d5eb67613402bb4fe
Styvensoft <steveen.echeverri@gmail.com>
2022-05-30 19:14:46 -0500

    First excercice fundamentals

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+             * Fundamentos de C#
+             * 
+             * Primitivos
+             * 
+             * Enteros - Reales - Booleanos
+             * 
+             * */
+            Console.WriteLine("Hello Welcome C#!");
+            Console.WriteLine("New comment");
+            int age;
+
+            age = 18;
+
+            Console.WriteLine(age);
+            // Double
+            Console.WriteLine(5.1 / 2.2);
+            // Residuo
+            Console.WriteLine(9 % 3);
+            //
+            int age_one = 19;
+
+            Console.WriteLine("Edad: " + age_one + "años");
+            
+            // Interpolacion
+            Console.WriteLine($"Tu edad es {age}");
+
+            // Incremento
+            Console.WriteLine(age++);
+
+
+            int agePerson1;
+            int agePerson2;
+            int agePerson3;
+
+            agePerson1 = agePerson2 = agePerson3 = 27;
+
+            Console.WriteLine(agePerson3);
+
+
+            Console.ReadKey();
+        }
+    }
+}
